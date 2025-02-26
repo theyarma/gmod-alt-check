9765fcad98dce51394fbdd94734c762761da17bb
theyarma <sp00py22110@gmail.com>
2025-02-26 12:44:18 -0600

    Removing unneccesary print statements

--- lua/autorun/server/sv_gmod-alt-check.lua
+++ lua/autorun/server/sv_gmod-alt-check.lua
@@ -17,26 +17,21 @@ hook.Add("PlayerInitialSpawn", "PlayerLog", function(ply)
   -- Checks if the players SteamID64 is in the table, and logs them if they arent. It will then print the table.
   if not playerData[ply:SteamID64()] then  
     table.insert(playerData, {plyName, sID64, ip})
-    PrintTable(playerData)
   else
-    print("Welcome "..plyName.." back to the server!")
-    PrintTable(playerData)
   
     local altFound = false
-    for k, v in ipairs(playerData) do    
+    for k, v in pairs(playerData) do    
       if v[2] ~= sID64 and v[3] == ip then
         print("Warning!"..ply.."has a duplicate IP and has been flagged as an Alt!")
         altFound = true
         table.insert(alts, ip)
       end
     end
-    
 
-    -- Prints the result of the for loop above.
     if altFound then
-      print("Alt Detected")
+      print("Alt found!")
     else
-      print("No Alt Detected")
+      print("No alts found!")
     end
   end
 end)
@@ -47,6 +42,7 @@ local function isAltDetected(ply)
   local ip = ply:IPAddress()
   local isAlt = table.HasValue(alts, ip)
   return isAlt
+  
 end
 
 hook.Add("PlayerInitialSpawn", "AltDetection", function()
