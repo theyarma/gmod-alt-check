0a21f1d9f3bc748baca26802f4efa04ff761e9a6
theyarma <sp00py22110@gmail.com>
2025-03-01 01:14:34 -0600

    prerelease

--- lua/autorun/server/sv_gmod-alt-check.lua
+++ lua/autorun/server/sv_gmod-alt-check.lua
@@ -10,7 +10,8 @@ hook.Add("PlayerInitialSpawn", "PlayerLog", function(ply)
   local sID64 = ply:SteamID64()
   local ip = ply:IPAddress()
   local altFound = false
-  
+
+
   -- Checks if the player's SteamID64 is in the table, and logs them if they aren't.
   if not playerData[sID64] then
 
@@ -20,15 +21,46 @@ hook.Add("PlayerInitialSpawn", "PlayerLog", function(ply)
     data.IP = ip
     data.NAME = plyName
     playerData[sID64] = data
+    print("Player Logged")
 
   end  
   
   for _, v in pairs(playerData) do
     
-    if v.IP ~= ip then continue end -- I like
+    if v.IP ~= ip then continue end
 
     local data = {}
     data.SteamID = sID64
     alts[ip] = data
+
+
   end
+
+
+  -- Chat Command
+
+  hook.Add("PlayerSay", "PlayerChat", function(ply, text)
+    
+    -- Variable Definitions
+    local args = string.Explode(" ", text, 2)
+    local targetIP = args[2]
+    local alts = alts[targetIP] or {}
+    alts = table.concat(alts, ", ")
+
+    -- Checks if the player is an admin.     
+      if not ply:IsAdmin() then 
+        ply:PrintMessage(HUD_PRINTTALK, "You are not an admin!") 
+        return 
+      end
+      
+      -- Prints out response based on alts table search.
+      if text == "!alts " and table.HasValue(alts, targetIP) then
+        ply:PrintMessage(HUD_PRINTTALK, "Alt Detected: " .. alts)     
+      else
+        print("No alt for".. alts)
+        ply:PrintMessage(HUD_PRINTTALK, "No Alts Detected for: " .. targetIP)  
+      end 
+  
+  end)
+
 end)
