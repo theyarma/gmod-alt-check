b00c265bbc159ca3548ab8ac334272b9763723e0
theyarma <sp00py22110@gmail.com>
2025-02-28 22:04:34 -0600

    Co-authored-by: Azara <azaraunknown@users.noreply.github.com>

--- lua/autorun/server/sv_gmod-alt-check.lua
+++ lua/autorun/server/sv_gmod-alt-check.lua
@@ -1,62 +1,34 @@
 -- Alt Checker
 -- Developed by: MM (theyarma)
-include "autorun/sh_gmod-alt-check.lua"
-AddCSLuaFile("vgui/vgui.lua")
+local playerData = {} -- Consider using a table keyed by SteamID64 instead of an array; this would allow constant-time lookups.
+local alts = {} -- Similarly, a dictionary keyed by IP could improve lookup efficiency in isAltDetected().
 
-local playerData = {}
-local alts = {}
 
--- Adds new players into the database. This resets every time you open the server.
 hook.Add("PlayerInitialSpawn", "PlayerLog", function(ply)
-       
-        --Variables
-        local plyName = ply:Nick()
-        local sID64 = ply:SteamID64()
-        local ip = ply:IPAddress()
-        local altFound = false
+  -- Variables
+  local plyName = ply:Nick()
+  local sID64 = ply:SteamID64()
+  local ip = ply:IPAddress()
+  local altFound = false
+  
+  -- Checks if the player's SteamID64 is in the table, and logs them if they aren't.
+  if not playerData[sID64] then
 
-        -- Checks if the players SteamID64 is in the table, and logs them if they arent.
-        if not playerData[ply:SteamID64()] then
-            table.insert(playerData, {plyName, sID64, ip})
-        end
+    if not ip then ip = 'Cannot Find' end
 
-        -- detects if an accounts IP matches but steamID64 does not and flags them as an alt, and stores them in a new table
-    for k, v in pairs(playerData) do
-        if v[2] ~= sID64 and v[3] == ip then
-                print("Warning!" .. ply .. "has a duplicate IP and has been flagged as an Alt!")
-                altFound = true
-                table.insert(alts, ip)
-        end
-    end
-end)
+    local data = {}
+    data.IP = ip
+    data.NAME = plyName
+    playerData[sID64] = data
 
-local function isAltDetected(ply)
-    -- Grabs IP then checks alt table if we have marked them as an alt.
-    local ip = ply:IPAddress()
-    local isAlt = table.HasValue(alts, ip)
-    return isAlt
-end
+  end  
+  
+  for _, v in pairs(playerData) do
+    
+    if v.IP ~= ip then continue end -- I like
 
-hook.Add("PlayerInitialSpawn", "AltDetection", function(ply)
-        local isAlt = isAltDetected(ply)
-
-        if isAlt then
-          print(ply:Nick() .. " has been detected as an alt. Please investigate.")
-        else
-          print(ply:Nick().." is not an alt.")
-        end
-
-end)
-
-
-util.AddNetworkString("CheckerButtonEvent")
-
-net.Receive("CheckerButtonEvent", function(len, ply)
-
-  local recievedData = net.ReadString()
-  print(ply:Nick().." clicked the button!")
-  net.Start("ReceivedData")
-  net.WriteString("Server recieved a message from: "..ply:Nick())
-  net.Send(ply)
-
+    local data = {}
+    data.SteamID = sID64
+    alts[ip] = data
+  end
 end)
